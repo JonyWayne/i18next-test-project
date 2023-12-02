1496093288d0144b67d08ed45791a852e7ad7241
Пирожков Евгений Евгеньевич (4060542) <eepirozhkov@dev.vtb.ru>
2023-12-02 13:56:24 +0300

    first commit/2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
+import Header from "./Components/Header";
+import Functional from "./Components/Functional";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>TEST i18next</p>
+        <Header />
+        <Functional />
       </header>
     </div>
   );
---
+++ src/Components/Functional.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Trans, useTranslation } from "react-i18next";
+import { Translation } from "react-i18next";
+
+const Functional = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="col-md-6">
+      <div className="card p-2">
+        <div className="card-body">
+          <h5 className="card-title">{t("content.functional")}</h5>
+
+          <Trans i18nKey="content.text" />
+          <Translation>
+            {(t, { i18n }) => <p>{t("content.text")}</p>}
+          </Translation>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Functional;
